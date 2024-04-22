d30b267fb4ef61c84ba5ffb2394f7eac2e69f0e6
ameralnas <amer.live477@gmail.com>
2024-04-22 17:15:13 +0200

    Try those

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -3,7 +3,7 @@ export default function getNeighborhoodsList() {
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood)
-    return self.sanFranciscoNeighborhoods; 
-  }
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }

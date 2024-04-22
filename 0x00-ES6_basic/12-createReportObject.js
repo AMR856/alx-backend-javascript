d008c0a4bc66f8d269c830ffef89245ef56a413e
ameralnas <amer.live477@gmail.com>
2024-04-22 18:32:16 +0200

    Enough now

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  const myObject = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: (employeesList) => (Object.keys(employeesList)).length,
+  };
+  return myObject;
+}

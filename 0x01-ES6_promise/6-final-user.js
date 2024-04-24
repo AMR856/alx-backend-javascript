6e4c73d1387ff747291f3bf56925f6b37880b5b6
ameralnas <amer.live477@gmail.com>
2024-04-24 21:31:36 +0200

    Still working on it

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -14,20 +14,14 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     });
 }
 
-// [
-//   {
-//     status: status_of_the_promise,
-//     value: value or error returned by the Promise
-//   },
-//   ...
-// ]
 
-// Promise.allSettled(promises).then((results) =>
-//   results.forEach((result) => console.log(result.status)),
-// );
-
-// Write and export a function named handleProfileSignup. It should 
-// accept three arguments firstName (string), lastName (string), and 
-// fileName (string). The function should call the two other functions. 
-// When the promises are all settled it 
-// should return an array with the following structure:
+// export default async function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise
+//     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+//     .then((res) => (
+//       res.map((o) => ({
+//         status: o.status,
+//         value: o.status === 'fulfilled' ? o.value : String(o.reason),
+//       }))
+//     ));
+// }

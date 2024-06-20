202faf5bffeb8e5d5b34e5b1a12691cfdb432e4d
ameralnas <amer.live477@gmail.com>
2024-06-20 18:17:32 +0300

    HOW

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,105 +1,134 @@
-const chai = require("chai");
-const expect = chai.expect;
-const calculateNumber = require('./1-calcul')
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
-describe('Adding subing dividing function test', function() {
-  it("Check basic without rouding", function() {
-    expect(calculateNumber(1, 3, 'SUM')).to.equal(4); 
+describe('calculateNumber', () => {
+  describe('type == "SUM"', () => {
+    it('equal positive numbers', () => {
+      expect(calculateNumber('SUM', 2.0, 2.0)).to.equal(4);
+    });
+
+    it('equal positive numbers (alternate)', () => {
+      expect(calculateNumber('SUM', 2.3, 1.8)).to.equal(4);
+    });
+
+    it('equal negative numbers', () => {
+      expect(calculateNumber('SUM', -2.0, -2.0)).to.equal(-4);
+    });
+
+    it('equal negative numbers (alternate)', () => {
+      expect(calculateNumber('SUM', -2.3, -1.8)).to.equal(-4);
+    });
+
+    it('negative and positive numbers', () => {
+      expect(calculateNumber('SUM', -2.0, 2.0)).to.equal(0);
+    });
+
+    it('positive and negative numbers', () => {
+      expect(calculateNumber('SUM', 2.0, -2.0)).to.equal(0);
+    });
+
+    it('0 and 0', () => {
+      expect(calculateNumber('SUM', 0.0, 0.0)).to.equal(0);
+    });
   });
 
-  it("Check if b is rounded", function() {
-    expect(calculateNumber(1, 3.7, 'SUM')).to.equal(5);
+  describe('type == "SUBTRACT"', () => {
+    it('equal positive numbers', () => {
+      expect(calculateNumber('SUBTRACT', 2.0, 2.0)).to.equal(0);
+    });
+
+    it('equal positive numbers (alternate)', () => {
+      expect(calculateNumber('SUBTRACT', 2.3, 1.8)).to.equal(0);
+    });
+
+    it('equal negative numbers', () => {
+      expect(calculateNumber('SUBTRACT', -2.0, -2.0)).to.equal(0);
+    });
+
+    it('equal negative numbers (alternate)', () => {
+      expect(calculateNumber('SUBTRACT', -2.3, -1.8)).to.equal(0);
+    });
+
+    it('negative and positive numbers', () => {
+      expect(calculateNumber('SUBTRACT', -2.0, 2.0)).to.equal(-4.0);
+    });
+
+    it('positive and negative numbers', () => {
+      expect(calculateNumber('SUBTRACT', 2.0, -2.0)).to.equal(4.0);
+    });
+
+    it('0 and 0', () => {
+      expect(calculateNumber('SUBTRACT', 0.0, 0.0)).to.equal(0);
+    });
   });
 
-  it('Check if a is rounded', function(){
-    expect(calculateNumber(1.2, 4, 'SUM')).to.equal(5);
-  });
+  describe('type == "DIVIDE"', () => {
+    it('positive numbers', () => {
+      expect(calculateNumber('DIVIDE', 8.0, 2.0)).to.equal(4.0);
+    });
 
-  it('Check if both are rounded', function(){
-    expect(calculateNumber(1.2, 3.7, 'SUM')).to.equal(5);
-  });
+    it('numbers with different signs', () => {
+      expect(calculateNumber('DIVIDE', -7.0, 2.0)).to.equal(-3.5);
+    });
 
-  it('Check if one is zero', function(){
-    expect(calculateNumber(0, 2, 'SUM')).to.equal(2);
-  });
+    it('numbers with different signs (alternate)', () => {
+      expect(calculateNumber('DIVIDE', 7.0, -2.0)).to.equal(-3.5);
+    });
 
-  it('Check if one is negative', function(){
-    expect(calculateNumber(-1, 3, 'SUM')).to.equal(2);
-  });
+    it('negative numbers', () => {
+      expect(calculateNumber('DIVIDE', -7.0, -2.0)).to.equal(3.5);
+    });
 
-  it('Check if both are negative', function() {
-    expect(calculateNumber(-2, -3, 'SUM')).to.equal(-5);
-  });
+    it('equal positive numbers', () => {
+      expect(calculateNumber('DIVIDE', 2.0, 2.0)).to.equal(1);
+    });
 
+    it('equal negative numbers', () => {
+      expect(calculateNumber('DIVIDE', -2.0, -2.0)).to.equal(1);
+    });
 
+    it('equal rounded up numbers', () => {
+      expect(calculateNumber('DIVIDE', 2.6, 3.0)).to.equal(1);
+    });
 
-  it("Check basic without rouding", function() {
-    expect(calculateNumber(1, 3, 'SUBTRACT')).to.equal(-2);
-  });
+    it('equal rounded down numbers', () => {
+      expect(calculateNumber('DIVIDE', 2.4, 2.0)).to.equal(1);
+    });
 
-  it("Check if b is rounded", function() {
-    expect(calculateNumber(3.7, 1, 'SUBTRACT')).to.equal(3);
-  });
+    it('0 and positive number', () => {
+      expect(calculateNumber('DIVIDE', 0.0, 5.0)).to.equal(0);
+    });
 
-  it('Check if a is rounded', function(){
-    expect(calculateNumber(4.2, 1, 'SUBTRACT')).to.equal(3);
-  });
+    it('0 and negative number', () => {
+      expect(calculateNumber('DIVIDE', 0.0, -5.0)).to.equal(-0);
+    });
 
-  it('Check if both are rounded', function(){
-    expect(calculateNumber(1.2, 3.7, 'SUBTRACT')).to.equal(-3);
-  });
+    it('positive number and 0', () => {
+      expect(calculateNumber('DIVIDE', 5.0, 0)).to.equal('Error');
+    });
 
-  it('Check if one is zero', function(){
-    expect(calculateNumber(0, 2, 'SUBTRACT')).to.equal(-2);
-  });
+    it('positive number and number rounded down to 0', () => {
+      expect(calculateNumber('DIVIDE', 5.0, 0.2)).to.equal('Error');
+    });
 
-  it('Check if one is negative', function(){
-    expect(calculateNumber(-1, 3, 'SUBTRACT')).to.equal(-4);
-  });
+    it('positive number and number rounded up to 0', () => {
+      expect(calculateNumber('DIVIDE', 5.0, -0.2)).to.equal('Error');
+    });
 
-  it('Check if both are negative', function() {
-    expect(calculateNumber(-2, -3, 'SUBTRACT')).to.equal(1);
-  });
+    it('negative number and 0', () => {
+      expect(calculateNumber('DIVIDE', -5.0, 0)).to.equal('Error');
+    });
 
+    it('negative number and number rounded down to zero', () => {
+      expect(calculateNumber('DIVIDE', -5.0, 0.2)).to.equal('Error');
+    });
 
+    it('negative number and number rounded up to zero', () => {
+      expect(calculateNumber('DIVIDE', -5.0, -0.2)).to.equal('Error');
+    });
 
-  it("Check basic without rouding", function() {
-    expect(calculateNumber(1, 3, 'DIVIDE')).to.equal(0.3333333333333333);
-  });
-
-  it("Check if b is rounded", function() {
-    expect(calculateNumber(3.7, 1, 'DIVIDE')).to.equal(4);
-  });
-
-  it('Check if a is rounded', function(){
-    expect(calculateNumber(2.2, 1, 'DIVIDE')).to.equal(2);
-  });
-
-  it('Check if both are rounded', function(){
-    expect(calculateNumber(1.2, 3.7, 'DIVIDE')).to.equal(0.25);
-  });
-
-  it('Check if one is zero', function(){
-    expect(calculateNumber(0, 2, 'DIVIDE')).to.equal(0);
-  });
-
-  it('Check if one is negative', function(){
-    expect(calculateNumber(-1, 3, 'DIVIDE')).to.equal(-0.3333333333333333);
-  });
-
-  it('Check if both are negative', function() {
-    expect(calculateNumber(-2, -3, 'DIVIDE')).to.equal(0.6666666666666666);
-  });
-
-  it('Check if one is zero', function(){
-    expect(calculateNumber(2, 0, 'DIVIDE')).to.equal('Error');
-  });
-
-  it('Check if one is negative', function(){
-    expect(calculateNumber(-1, 0, 'DIVIDE')).to.equal('Error');
-  });
-
-  it('Check if both are negative', function() {
-    expect(calculateNumber(-1, 0.4, 'DIVIDE')).to.equal('Error');
+    it('0 and 0', () => {
+      expect(calculateNumber('DIVIDE', 0.0, 0.0)).to.equal('Error');
+    });
   });
 });

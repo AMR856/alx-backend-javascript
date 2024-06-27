c49a9a4299db9a052fd2c84cbfd17fd65fe22da9
ameralnas <amer.live477@gmail.com>
2024-06-27 20:59:19 +0300

    Finished thi

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 
-
 const countStudents = (path) => {
-  try{
+  try {
     const data = fs.readFileSync(path, 'utf-8');
-    const splittedData = data.split('\n').slice(1,);
+    const splittedData = data.split('\n').slice(1);
     console.log(`Number of students: ${splittedData.length}`);
-    const studentsDict  = {};
+    const studentsDict = {};
     const specDict = {};
     for (const studentString of splittedData) {
       const studentList = studentString.split(',');
-      if (!Object.keys(specDict).includes(studentList[3])){
+      if (!Object.keys(specDict).includes(studentList[3])) {
         specDict[studentList[3]] = 1;
         studentsDict[`${studentList[3]}_students`] = [];
         studentsDict[`${studentList[3]}_students`].push(studentList[0]);
@@ -20,11 +19,11 @@ const countStudents = (path) => {
       }
     }
     for (const specKey of Object.keys(specDict)) {
-      const stringThing = studentsDict[Object.keys(studentsDict).find(key => key === `${specKey}_students`)].join(', ');
+      const stringThing = studentsDict[Object.keys(studentsDict).find((key) => key === `${specKey}_students`)].join(', ');
       console.log(`Number of students in ${specKey}: ${specDict[specKey]}. List: ${stringThing}`);
     }
-  } catch(error) {
-    throw new Error("Cannot load the database");
+  } catch (error) {
+    throw new Error('Cannot load the database');
   }
 };
 
--- 0x05-Node_JS_basic/2-read_file.test.js
+++ 0x05-Node_JS_basic/2-read_file.test.js
@@ -1,4 +1,4 @@
-const { ShouldThrow, expect } = require('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
 
 const countStudents = require('./2-read_file.js');
@@ -14,11 +14,7 @@ describe('countStudents', () => {
     consoleSpy.restore();
   });
 
-  it('logs to the console the right messages', () => {
-    countStudents('./database.csv');
-
-    expect(consoleSpy.calledWith('Number of students: 10')).to.be.true;
-    expect(consoleSpy.calledWith('Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie')).to.be.true;
-    expect(consoleSpy.calledWith('Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy')).to.be.true;
+  it('throws the correct error message', () => {
+    expect(() => countStudents('./blabl.csv')).to.throw('Cannot load the database');
   });
 });

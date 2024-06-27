017403637764cee0c930311a589d090eb80f31eb
ameralnas <amer.live477@gmail.com>
2024-06-27 20:55:29 +0300

    Hi

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,51 +1,31 @@
-const fs = require('node:fs');
+const fs = require('fs');
 
 
 const countStudents = (path) => {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    myDataArray = data.split('\n').slice(1);
-    const myDataOfLists = myDataArray.map(entry => entry.split(','));
-    console.log(`Number of students: ${myDataArray.length}`);
-    myDictionay  = {};
-    let spec = '';
-    let theStudentsListKey = [];
-    for (const student of myDataOfLists) {
-      spec = student[3];
-      theKey = spec + '_number';
-      if (theKey in myDictionay) {
-        myDictionay[theKey] = myDictionay[theKey] + 1;
-        theStudentsListKey = spec + '_array';
-        myDictionay[theStudentsListKey].push(student[0]);
+  try{
+    const data = fs.readFileSync(path, 'utf-8');
+    const splittedData = data.split('\n').slice(1,);
+    console.log(`Number of students: ${splittedData.length}`);
+    const studentsDict  = {};
+    const specDict = {};
+    for (const studentString of splittedData) {
+      const studentList = studentString.split(',');
+      if (!Object.keys(specDict).includes(studentList[3])){
+        specDict[studentList[3]] = 1;
+        studentsDict[`${studentList[3]}_students`] = [];
+        studentsDict[`${studentList[3]}_students`].push(studentList[0]);
       } else {
-        myDictionay[theKey] = 1;
-        theStudentsListKey = spec + '_array';
-        myDictionay[theStudentsListKey] = Array();
-        myDictionay[theStudentsListKey].push(student[0]);
+        specDict[studentList[3]] += 1;
+        studentsDict[`${studentList[3]}_students`].push(studentList[0]);
       }
     }
-    let finalDict = {};
-    for (const [key, value] of Object.entries(myDictionay)) {
-      spec1 = key.split('_');
-      let counter = 0;
-      if (spec1[1] === 'number') {
-        finalDict[spec1[0]] = myDictionay[spec1[0] + '_array'];
-        process.stdout.write(`Number of students in ${spec1[0]}: ${finalDict[spec1[0]].length}. List: `);
-        for (const [key1, value1] of Object.entries(finalDict[spec1[0]])) {
-          if (counter === finalDict[spec1[0]].length - 1) {
-            process.stdout.write(`${value1}`);
-          } else {
-            process.stdout.write(`${value1}, `);
-          }
-          counter = counter + 1;
-        }
-        counter = 0;
-        console.log();
-      }
+    for (const specKey of Object.keys(specDict)) {
+      const stringThing = studentsDict[Object.keys(studentsDict).find(key => key === `${specKey}_students`)].join(', ');
+      console.log(`Number of students in ${specKey}: ${specDict[specKey]}. List: ${stringThing}`);
     }
-  } catch (err) {
+  } catch(error) {
     throw new Error("Cannot load the database");
   }
-}
+};
 
 module.exports = countStudents;
